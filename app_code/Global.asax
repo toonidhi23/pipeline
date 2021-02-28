012052fffcb2e406d97af0745cbbbd557bfefb95
toonidhi <toonidhi@gmail.com>
2021-02-28 14:18:01 -0500

    Changes made

---
+++ app_code/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="app_code.Global" %>
---
+++ app_code/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace app_code
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
